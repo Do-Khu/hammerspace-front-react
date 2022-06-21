9013599639517f21274631a763eb5ce7f86ce664
Cybergoat9152 <arthurtbarros@outlook.com>
2022-06-21 13:41:56 -0300

    feat: adding router interface

---
+++ hammerspace-app/src/Routes/router.ts
@@ -0,0 +1,16 @@
+import IRoute from "../Interfaces/router";
+
+const routes: IRoute[] = [
+    {
+        path:'/login',
+        name:'login page',
+        component: 'TODO',
+        exact: true
+    },
+    {
+        path:'/home',
+        name:'home page',
+        component: 'TODO',
+        exact: true
+    }
+];
